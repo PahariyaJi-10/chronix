c40be57cb0ead6ec2e33f64848f57680dd549ea1
Divyansh Pahariya <divyanshpahariya@gmail.com>
2026-07-21 12:17:33 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/divyansh/chronix/ChronixApplication.java
@@ -0,0 +1,13 @@
+package com.divyansh.chronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyansh/chronix/ChronixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.chronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
