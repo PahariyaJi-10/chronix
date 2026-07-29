bb1387e5b60170881764418d2fed54dd92f95c5a
Divyansh Pahariya <divyanshpahariya@gmail.com>
2026-07-29 11:30:07 +0530

    feat: add job request and response DTOs

--- src/main/java/com/divyansh/chronix/dto/CreateJobRequest.java
+++ src/main/java/com/divyansh/chronix/dto/CreateJobRequest.java
@@ -1,5 +1,47 @@
 package com.divyansh.chronix.dto;
 
+import com.divyansh.chronix.entity.JobType;
+import java.time.LocalDateTime;
+
 public class CreateJobRequest {
 
+    private String name;
+    private JobType type;
+    private LocalDateTime scheduledAt;
+    private String payload;
+
+    public CreateJobRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public JobType getType() {
+        return type;
+    }
+
+    public void setType(JobType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getScheduledAt() {
+        return scheduledAt;
+    }
+
+    public void setScheduledAt(LocalDateTime scheduledAt) {
+        this.scheduledAt = scheduledAt;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
 }
--- src/main/java/com/divyansh/chronix/dto/JobResponse.java
+++ src/main/java/com/divyansh/chronix/dto/JobResponse.java
@@ -1,5 +1,38 @@
 package com.divyansh.chronix.dto;
 
+import com.divyansh.chronix.entity.JobStatus;
+import com.divyansh.chronix.entity.JobType;
+
 public class JobResponse {
-    
+
+    private Long id;
+    private String name;
+    private JobType type;
+    private JobStatus status;
+
+    public JobResponse() {
+    }
+
+    public JobResponse(Long id, String name, JobType type, JobStatus status) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public JobType getType() {
+        return type;
+    }
+
+    public JobStatus getStatus() {
+        return status;
+    }
 }
