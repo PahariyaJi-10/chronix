90e927bf44a7881577b73b99b654e082df834236
Divyansh Pahariya <divyanshpahariya@gmail.com>
2026-08-06 12:51:08 +0530

    feat: implement automatic scheduled job execution

--- src/main/java/com/divyansh/chronix/repository/JobRepository.java
+++ src/main/java/com/divyansh/chronix/repository/JobRepository.java
@@ -1,10 +1,16 @@
 package com.divyansh.chronix.repository;
 
 import com.divyansh.chronix.entity.Job;
+import com.divyansh.chronix.entity.JobStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface JobRepository extends JpaRepository<Job, Long> {
 
+    List<Job> findByStatusAndScheduledAtLessThanEqual(
+            JobStatus status,
+            LocalDateTime scheduledAt
+    );
 }
--- src/main/java/com/divyansh/chronix/scheduler/JobScheduler.java
+++ src/main/java/com/divyansh/chronix/scheduler/JobScheduler.java
@@ -1,14 +1,60 @@
 package com.divyansh.chronix.scheduler;
 
+import com.divyansh.chronix.entity.Job;
+import com.divyansh.chronix.entity.JobStatus;
+import com.divyansh.chronix.repository.JobRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class JobScheduler {
 
+    private final JobRepository jobRepository;
+
+    public JobScheduler(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
     @Scheduled(fixedRate = 10000)
     public void executeJobs() {
 
-        System.out.println("Scheduler is checking for pending jobs...");
+        List<Job> pendingJobs =
+                jobRepository.findByStatusAndScheduledAtLessThanEqual(
+                        JobStatus.PENDING,
+                        LocalDateTime.now()
+                );
+
+        if (pendingJobs.isEmpty()) {
+            System.out.println("No pending jobs found.");
+            return;
+        }
+
+        for (Job job : pendingJobs) {
+
+            System.out.println("Executing Job: " + job.getName());
+
+            job.setStatus(JobStatus.RUNNING);
+            jobRepository.save(job);
+
+            try {
+
+                Thread.sleep(2000);
+
+                job.setStatus(JobStatus.COMPLETED);
+
+            } catch (Exception e) {
+
+                job.setStatus(JobStatus.FAILED);
+            }
+
+            job.setUpdatedAt(LocalDateTime.now());
+
+            jobRepository.save(job);
+
+            System.out.println("Completed Job: " + job.getName());
+        }
     }
 }
